76f96a9e9f9cbb3b6af81db620137fee88f55a0d
Marcin Fortuna <marcin.fortuna@onet.eu>
2024-07-29 20:05:37 +0200

    feat: add chromosome methods

--- core/chromosome.py
+++ core/chromosome.py
@@ -1,14 +1,16 @@
 import numpy as np
 
-class chromosome:
-    def __init__() :
+class Chromosome:
+    def __init__(self, size, boundaries):
+        self._lower_bound = boundaries[0]
+        self._upper_bound = boundaries[1]
+        self._chromosome = np.zeros(size)
+
+    def __str__(self):
+        return f"chromosome: {self.get_binary_representation()}"
+
+    def get_binary_representation(self):
         pass
 
-    def __str__():
-        pass
-
-    def get_binary_representation():
-        pass
-
-    def get_decimal_representation():
-        pass
+    def get_decimal_representation(self):
+        return self._chromosome
