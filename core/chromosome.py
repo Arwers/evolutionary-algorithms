55d116e0fdaed1ac85632ca22cd73cbdbb597120
Marcin Fortuna <marcin.fortuna@onet.eu>
2024-07-29 18:17:18 +0200

    feat: add chromosome class

---
+++ core/chromosome.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class chromosome:
+    def __init__() :
+        pass
+
+    def __str__():
+        pass
+
+    def get_binary_representation():
+        pass
+
+    def get_decimal_representation():
+        pass
