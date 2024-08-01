9906970760b4564eb2fb80184ecf025d72e92b5b
Marcin Fortuna <marcin.fortuna@onet.eu>
2024-08-01 08:32:21 +0200

    feat: add new test case

--- core/test_individual.py
+++ core/test_individual.py
@@ -1,11 +1,12 @@
 import pytest
+import numpy as np
 from individual import Individual
 
 class TestIndividual:
     def setup_method(self):
-        self.individual = Individual(genes=5, precision=6, bundaries=(-5, 5))
+        self.individual = Individual(genes=5, boundaries=(-5, 5), precision=6)
 
-    def test_setter():
-        have = Individual.get_size()
+    def test_size(self):
+        have = self.individual.get_size()
         want = 5
         assert have == want
