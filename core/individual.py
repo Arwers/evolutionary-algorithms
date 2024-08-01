ce44d699f30c4b5a4b6cec33c54245c8fea68241
Marcin Fortuna <marcin.fortuna@onet.eu>
2024-08-01 08:32:09 +0200

    feat: add getters

--- core/individual.py
+++ core/individual.py
@@ -1,2 +1,13 @@
+import numpy as np
+from chromosome import Chromosome
+
 class Individual:
-    pass
+    def __init__(self, genes, boundaries, precision):
+        self.genes = genes
+        self.individual = np.array([Chromosome(boundaries, precision) for _ in range(genes)])
+
+    def get_size(self):
+        return self.genes
+    
+    def get_individual(self):
+        return self.individual
