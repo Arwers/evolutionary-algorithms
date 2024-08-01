d46510e1d4d109d4530a86c95f04c3faf63e096a
Marcin Fortuna <marcin.fortuna@onet.eu>
2024-08-01 08:41:10 +0200

    feat: add __str__ implementation

--- core/individual.py
+++ core/individual.py
@@ -1,10 +1,17 @@
 import numpy as np
-from chromosome import Chromosome
+from .chromosome import Chromosome
 
 class Individual:
     def __init__(self, genes, boundaries, precision):
         self.genes = genes
         self.individual = np.array([Chromosome(boundaries, precision) for _ in range(genes)])
+    
+    def __str__(self):
+        string_repr = ""
+        for chr in self.get_individual():
+            string_repr += f"{chr.decode_chromosome()}\n"
+        return string_repr
+            
 
     def get_size(self):
         return self.genes
