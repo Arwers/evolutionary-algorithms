d9cb8c5dd9d31e410f1f9003e0ee076617518c76
Marcin Fortuna <marcin.fortuna@onet.eu>
2024-08-01 09:54:03 +0200

    fix: main cleanup

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import core.chromosome
+import core.individual
 def main():
-    chr = core.chromosome.Chromosome((-5, 5), 6)
-    print(chr.get_chromosome())
-    print(chr.decode_chromosome())
+    pass
 
 if __name__ == "__main__":
     main()
