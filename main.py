fb68a88a6bcac02cf13ca11a26d10b42a6c191cf
Marcin Fortuna <marcin.fortuna@onet.eu>
2024-08-10 15:42:46 +0200

    feat: new main func

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import core.chromosome
 import core.individual
-def main():
-    pass
+import core.population
 
+def main():
+    pop = core.population.Population((-5,5), 6, 2, 5)
+    pop.evaluate_population(lambda x, y: x + y)
+    for ind in pop.get_population():
+        print(ind)
+
+    print()
+    print(f"Best: \n {pop.get_best()} \n")    
+    print(f"Mean: \n {pop.get_mean()}")
 if __name__ == "__main__":
     main()
